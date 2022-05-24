170a83edec37c0cd462f6f7fdffe4f4b2b5f7590
Júlia Jóia <105455881+julia-joiab@users.noreply.github.com>
2022-05-24 15:10:52 -0300

    empréstimo
    códigos iniciais

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function App () {
+
+    require ("./App.css")
+
+    return(
+
+        <div>
+
+            <h1>oi</h1>
+            <button> enter </button>
+
+        </div>
+    );
+
+}
+export default App;
+
+/// Cor amarela: #daa520
